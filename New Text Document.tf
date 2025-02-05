956973d7b187660eee44a3d5fbcd11e98bad7948
Mahmoud Hesham <mahmoud.hesham@fortecloud.com>
2025-02-05 15:37:07 +0200

    adding s3 bucket

---
+++ New Text Document.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-terraform-s3-bucket-12345"  # Change to a unique name
+  acl    = "private"
+
+  tags = {
+    Name        = "MyTerraformBucket1446121999"
+    Environment = "Dev"
+  }
+}
+
+output "s3_bucket_name" {
+  value = aws_s3_bucket.my_bucket.id
+}
